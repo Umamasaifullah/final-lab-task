4b252d01531222cfd0f7602d4002db6d782ebe87
Umamasaifullah <93112161+Umamasaifullah@users.noreply.github.com>
2021-11-30 13:52:08 +0500

    this is my final lab

---
+++ final lab/ConsoleApplication1/ConsoleApplication1/HourlyEmployee.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class HourlyEmployee
+    {
+        public int hourlyrateofpay
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int EmployeeID
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int EmployeeName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int EmployeeType
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Hoursworked
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void CalculateSalary()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
